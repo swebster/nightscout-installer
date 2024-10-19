f7715838930de578622cd2785d917265ef1042a8
Stuart Webster <sw_webster@hotmail.com>
2024-10-20 01:01:52 +0200

    feat: extend podman user script to enable podman compose

--- add_podman_user.sh
+++ add_podman_user.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-PODMAN_HOME='/home/podman'
+readonly PODMAN_HOME='/home/podman'
+readonly PODMAN_BIN="${PODMAN_HOME}/.local/bin"
+readonly DOCKER_DOWNLOADS='https://github.com/docker/compose/releases/download'
+readonly COMPOSE_VERSION='v2.29.6'
+readonly TASK_VERSION='v3.39.2'
 
 function assign_subids() {
   mapfile -t -d: subuids < <(tail -1 /etc/subuid | cut -d: -f2,3)
@@ -38,8 +41,19 @@ if ! grep -q podman /etc/subuid; then
   assign_subids
 fi
 
-if ! sudo -u podman test -x "${PODMAN_HOME}/.local/bin/task"; then
-  sudo install -o podman -g nogroup -t "${PODMAN_HOME}" "${SCRIPT_DIR}/install_task.sh"
-  sudo -u podman "${PODMAN_HOME}/install_task.sh"
+if ! sudo -u podman test -x "${PODMAN_BIN}/task"; then
+  sudo -u podman curl -L https://taskfile.dev/install.sh -o "${PODMAN_HOME}/install_task.sh"
+  sudo -u podman chmod +x "${PODMAN_HOME}/install_task.sh"
+  sudo -u podman "${PODMAN_HOME}/install_task.sh" -d -b "${PODMAN_BIN}" "${TASK_VERSION}"
   sudo -u podman rm -f "${PODMAN_HOME}/install_task.sh"
+fi
+
+if ! sudo -u podman test -x "${PODMAN_BIN}/docker-compose"; then
+  sudo -u podman curl -L \
+    "${DOCKER_DOWNLOADS}/${COMPOSE_VERSION}/docker-compose-linux-x86_64" -o \
+    "${PODMAN_BIN}/docker-compose"
+  sudo -u podman chmod +x "${PODMAN_BIN}/docker-compose"
+  sudo -u podman systemctl --user enable --now podman.socket
+  sudo -u podman -i sh -c \
+    'printf "\nexport DOCKER_HOST=unix://${XDG_RUNTIME_DIR%/}/podman/podman.sock\n" >> ~/.profile'
 fi

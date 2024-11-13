d89bde2b1ca7940768c401ac41e89d6cf13633cc
Stuart Webster <sw_webster@hotmail.com>
2024-11-13 02:24:29 +0100

    chore: customise podlet flags for the local podman

--- configure_quadlet.sh
+++ configure_quadlet.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-# generate intermediate compose file with interpolated variables
 trap 'rm -f docker-compose.config.yml' EXIT
 
+# generate intermediate compose file with interpolated variables
 $(which podman || which docker) compose \
-  -f docker-compose.yml config --no-normalize -o docker-compose.config.yml
+  -f docker-compose.yml -f docker-compose.networks.yml \
+  config --no-normalize -o docker-compose.config.yml
 
 # revert .services.[].ports and volumes to short syntax for compatibility with podlet
-# https://github.com/mikefarah/yq/#install
 yq -Pi '.services *= (load("docker-compose.yml") |
     # just select the ports and volumes from the original file
     .services | .[] |= pick(["ports", "volumes"])
   )' docker-compose.config.yml
 
+# generate the appropriate podlet flags for the local version of podman
+podman_version=$(which podman >/dev/null && podman --version || echo 'podman version 4.9.3')
+major_minor=$(sed -n 's/podman version //p' <<< "${podman_version}" | cut -d. -f -2)
+podlet_schema="${major_minor/4.9/4.8}"
+pod_flag=$(test "${major_minor%%.*}" -ge 5 && echo --pod)
+
 # generate quadlet configuration from the intermediate compose file
-# https://github.com/containers/podlet?tab=readme-ov-file#install
-podlet compose --pod docker-compose.config.yml
+podlet -p "${podlet_schema}" compose ${pod_flag:+"$pod_flag"} docker-compose.config.yml
